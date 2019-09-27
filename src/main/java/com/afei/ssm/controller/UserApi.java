0ac9b0927971cddbddee0e0e5faace7aec24d6ea
chenxiangming <19802021432@139.com>
2019-09-27 14:04:23 +0800

    完善了一下

--- src/main/java/com/afei/ssm/controller/UserApi.java
+++ src/main/java/com/afei/ssm/controller/UserApi.java
@@ -2,6 +2,7 @@ package com.afei.ssm.controller;
 
 import com.afei.ssm.entity.User;
 import com.afei.ssm.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * created by cxming on 2019/9/26
  */
 @RestController
+@Slf4j
 public class UserApi {
 
     @Autowired
@@ -18,6 +20,7 @@ public class UserApi {
 
     @GetMapping("/getUser")
     public User getUser(Integer id) {
+        log.info("getUser");
         return userService.getUser(id);
     }
 
